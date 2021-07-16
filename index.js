80cfb198eb69fbe315dd975d89139cb2747f340d
yait-ucess <bandai28@ezweb.ne.jp>
2021-07-16 22:42:41 +0900

    前コミットのIssue（       - Issue -     本来、指定のheight内（timerの下にあるボタンに被らないように設定・制限した高さ内）で、スクロールすることによって開始時間の選択をしてもらいたいが、     現状全ての選択肢が一気に表示され、ボタンに被ってしまっていることでUIが崩れている。     次はこのIssueを解決していく。 ） を解決した。
    時間部分をクリックした際の、スタート時間の選択肢の表示・非表示がキレイに実装できた。
    が、ブラウザ上では何の問題もないが、
    
    - Issue  -
    consoleではindex.js:11 Uncaught TypeError: Cannot read property 'classList' of undefinedat HTMLButtonElement.<anonymous>が発生しているので、
    次回はこれを解決、もしくは一旦無視して次に進む。

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const clock_edit = document.getElementById("clock_edit");
 const clock_select = document.getElementById("clock_select");
+const clock_select_btn = document.getElementsByClassName("clock_select_btn");
 
 clock_edit.addEventListener("click", () => {
   clock_select.classList.toggle("clock_select");
-  // console.log(element.classList);
-  // this.classList.toggle("clock_select");
+  console.log(clock_select.classList);
+  console.log(clock_edit);
+
+  for (let i = 0; clock_select_btn.length; i++) {
+    clock_select_btn[i].classList.toggle("is_open");
+  }
 });
+
+
