ca932bea314536370254c469bd65c58275ef7618
yait-ucess <bandai28@ezweb.ne.jp>
2021-07-17 22:30:57 +0900

    開始時間の選択肢からスタート時間をクリックすると、その時間がメインの時間に設定されるようにした。
    さらに、メインの時間が設定されたら、選択肢が閉じるようにした。

--- index.js
+++ index.js
@@ -1,15 +1,4 @@
-const clock_edit = document.getElementById("clock_edit");
-const clock_select = document.getElementById("clock_select");
-const clock_select_btn = document.getElementsByClassName("clock_select_btn");
+import { toggle_select, select_time } from "./module/clock.js";
 
-clock_edit.addEventListener("click", () => {
-  clock_select.classList.toggle("clock_select");
-  console.log(clock_select.classList);
-  console.log(clock_edit);
-
-  for (let i = 0; clock_select_btn.length; i++) {
-    clock_select_btn[i].classList.toggle("is_open");
-  }
-});
-
-
+toggle_select();
+select_time();
---
+++ module/clock.js
@@ -0,0 +1,86 @@
+const clock_edit = document.getElementById("clock_edit");
+const clock_select = document.getElementById("clock_select");
+const clock_select_btn = document.getElementsByClassName("clock_select_btn");
+
+const toggle_is_open = () => {
+  clock_select.classList.toggle("clock_select");
+  for (let i = 0; clock_select_btn.length; i++) {
+    clock_select_btn[i].classList.toggle("is_open");
+  };
+};
+
+const toggle_select = () => {
+  clock_edit.addEventListener("click", () => {
+    toggle_is_open();
+  });  
+};
+
+const select_time = () => {
+  clock_select_btn[0].addEventListener("click", () => {
+    let select_time = clock_select_btn[0].innerHTML;
+    clock_edit.innerHTML = select_time;
+    toggle_is_open();
+  });
+  
+  clock_select_btn[1].addEventListener("click", () => {
+    let select_time = clock_select_btn[1].innerHTML;
+    clock_edit.innerHTML = select_time;
+    toggle_is_open();
+  });
+  
+  clock_select_btn[2].addEventListener("click", () => {
+    let select_time = clock_select_btn[2].innerHTML;
+    clock_edit.innerHTML = select_time;
+    toggle_is_open();
+  });
+  
+  clock_select_btn[3].addEventListener("click", () => {
+    let select_time = clock_select_btn[3].innerHTML;
+    clock_edit.innerHTML = select_time;
+    toggle_is_open();
+  });
+  
+  clock_select_btn[4].addEventListener("click", () => {
+    let select_time = clock_select_btn[4].innerHTML;
+    clock_edit.innerHTML = select_time;
+    toggle_is_open();
+  });
+  
+  clock_select_btn[5].addEventListener("click", () => {
+    let select_time = clock_select_btn[5].innerHTML;
+    clock_edit.innerHTML = select_time;
+    toggle_is_open();
+  });
+  
+  clock_select_btn[6].addEventListener("click", () => {
+    let select_time = clock_select_btn[6].innerHTML;
+    clock_edit.innerHTML = select_time;
+    toggle_is_open();
+  });
+  
+  clock_select_btn[7].addEventListener("click", () => {
+    let select_time = clock_select_btn[7].innerHTML;
+    clock_edit.innerHTML = select_time;
+    toggle_is_open();
+  });
+  
+  clock_select_btn[8].addEventListener("click", () => {
+    let select_time = clock_select_btn[8].innerHTML;
+    clock_edit.innerHTML = select_time;
+    toggle_is_open();
+  });
+  
+  clock_select_btn[9].addEventListener("click", () => {
+    let select_time = clock_select_btn[9].innerHTML;
+    clock_edit.innerHTML = select_time;
+    toggle_is_open();
+  });
+  
+  clock_select_btn[10].addEventListener("click", () => {
+    let select_time = clock_select_btn[10].innerHTML;
+    clock_edit.innerHTML = select_time;
+    toggle_is_open();
+  });
+};
+
+export { toggle_select, select_time };
