3ba68e467d2945e4ee54773e9f82abaff35be846
yait-ucess <bandai28@ezweb.ne.jp>
2021-07-19 17:53:06 +0900

    Pause機能実装
    playボタンをクリックすると、タイマーがスタートし、play → pauseボタンに切り替わる。
    pauseボタンをクリックすると、タイマーがストップし、pause → playボタンに切り替わる。
    
    - Issue -
    現状、pause中にplayボタンをクリックすると、00:00からのスタートになってしまうので、停止中の時間から再開できるようにする。

--- index.js
+++ index.js
@@ -1,17 +1,37 @@
 import { toggle_select, select_time } from "./module/clock.js";
 
 const clock_edit = document.getElementById("clock_edit");
-// const play_start = document.getElementById("play_start");
+const play_start = document.getElementById("play_start");
+const play_btn = document.getElementById("play");
 
 toggle_select();
 select_time();
 
 let start;
+let interval_id;
 
 play_start.addEventListener("click", () => {
-  start = new Date();
+  if (play_btn.classList[2] === "fa-play-circle") {
+    start = new Date();
+    console.log(play_start);
+    console.log(play_btn);
+    console.log(play_btn.classList);
 
-  setInterval(goClock, 10);
+    interval_id = setInterval(goClock, 1000);
+
+    // pauseボタンに切り替え
+    play_btn.classList.remove("fa-play-circle");
+    play_btn.classList.add("fa-pause-circle");
+  } else {
+    clearInterval(interval_id);
+
+    // playボタンに切り替え
+    play_btn.classList.remove("fa-pause-circle");
+    play_btn.classList.add("fa-play-circle");
+  }
+
+  console.log(play_btn.classList[2]);
+
 });
 
 const addZero = (value) => {
@@ -30,10 +50,19 @@ const goClock = () => {
   let minutes = Math.floor(seconds / 60);
 
   seconds = seconds - minutes * 60;
+  // seconds = seconds + parseInt(clock_edit.innerHTML);
+  console.log(seconds);
+  // console.log(clock_edit.innerHTML);
+  console.log(parseInt(document.getElementById("clock_edit").innerHTML));
+  // console.log(seconds + parseInt(clock_edit.innerHTML));
 
   seconds = addZero(seconds);
   minutes = addZero(minutes);
+  // console.log(seconds);
+  // console.log(minutes);
+  // console.log(clock_edit.innerHTML);
 
-  // clock_edit.innerHTML = now.getTime() - start.getTime();
   clock_edit.innerHTML = minutes + ":" + seconds;
+  // console.log(clock_edit.innerHTML);
+  seconds = seconds + parseInt(clock_edit.innerHTML);
 };
