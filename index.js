bfe7c04f62d9dc21c82b4b3be5d752dc91092989
yait-ucess <bandai28@ezweb.ne.jp>
2021-07-20 07:05:21 +0900

    指定した時間から１秒ずつ減るカウント機能を実装
    下記の四則演算子を使って実装。
    
    let time = parseInt(clock_edit.innerHTML) * 60 - 1;
    let seconds = Math.floor(time % 60);
    let minutes = Math.floor(time / 60);
    
    -Issue-
    ・00:00後に、-1:-1のように表示されてしまうので、00:00のままでストップする処理を追加する。
    ・pause後のreplayの挙動がおかしいので直す。

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import { toggle_select, select_time } from "./module/clock.js";
 
-const clock_edit = document.getElementById("clock_edit");
+let clock_edit = document.getElementById("clock_edit");
 const play_start = document.getElementById("play_start");
 const play_btn = document.getElementById("play");
 
@@ -12,16 +12,31 @@ let interval_id;
 
 play_start.addEventListener("click", () => {
   if (play_btn.classList[2] === "fa-play-circle") {
-    start = new Date();
-    console.log(play_start);
-    console.log(play_btn);
-    console.log(play_btn.classList);
+    // start = new Date();
+    let time = parseInt(clock_edit.innerHTML) * 60 - 1;
+    console.log(time);
 
-    interval_id = setInterval(goClock, 1000);
+    // interval_id = setInterval(goClock, 1000);
+
+    interval_id = setInterval(() => {
+      let seconds = Math.floor(time % 60);
+      let minutes = Math.floor(time / 60);
+
+      seconds = addZero(seconds);
+      minutes = addZero(minutes);
+
+      console.log(time);
+      console.log(seconds);
+      console.log(minutes);
+
+      clock_edit.innerHTML = minutes + ":" + seconds;
+      time--;
+   }, 1000);
 
     // pauseボタンに切り替え
     play_btn.classList.remove("fa-play-circle");
     play_btn.classList.add("fa-pause-circle");
+
   } else {
     clearInterval(interval_id);
 
@@ -29,40 +44,26 @@ play_start.addEventListener("click", () => {
     play_btn.classList.remove("fa-pause-circle");
     play_btn.classList.add("fa-play-circle");
   }
-
-  console.log(play_btn.classList[2]);
-
 });
 
 const addZero = (value) => {
   if (value < 10) {
     value = "0" + value;
   }
-
   return value;
 }
 
-const goClock = () => {
-  const now = new Date();
+// const goClock = () => {
+//   const now = new Date();
 
-  let ms = now.getTime() - start.getTime();
-  let seconds = Math.floor(ms / 1000);
-  let minutes = Math.floor(seconds / 60);
+//   let ms = now.getTime() - start.getTime();
+//   let seconds = Math.floor(ms / 1000);
+//   let minutes = Math.floor(seconds / 60);
 
-  seconds = seconds - minutes * 60;
-  // seconds = seconds + parseInt(clock_edit.innerHTML);
-  console.log(seconds);
-  // console.log(clock_edit.innerHTML);
-  console.log(parseInt(document.getElementById("clock_edit").innerHTML));
-  // console.log(seconds + parseInt(clock_edit.innerHTML));
+//   seconds = seconds - minutes * 60;
 
-  seconds = addZero(seconds);
-  minutes = addZero(minutes);
-  // console.log(seconds);
-  // console.log(minutes);
-  // console.log(clock_edit.innerHTML);
+//   seconds = addZero(seconds);
+//   minutes = addZero(minutes);
 
-  clock_edit.innerHTML = minutes + ":" + seconds;
-  // console.log(clock_edit.innerHTML);
-  seconds = seconds + parseInt(clock_edit.innerHTML);
-};
+//   clock_edit.innerHTML = minutes + ":" + seconds;
+// };
