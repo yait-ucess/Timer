5b2b0d0390a53a97ff067eb19396f5ec8b8c84c6
yait-ucess <bandai28@ezweb.ne.jp>
2021-07-21 23:52:43 +0900

    スタート時間選択時に、選択した時間のボタンclassListに、"is_current"を付与。
    clock_select_btnを選択時に、選択したボタンにclass="is_current"を付与。
    選択前のボタンのclass="is_current"は削除（つまりis_currentは一意）。
    デフォルトは<button class="clock_select_btn is_current">10:00</button>。

--- index.js
+++ index.js
@@ -1,41 +1,53 @@
-import { toggle_select, select_time } from "./module/clock.js";
+import { toggle_is_open, toggle_select, select_time } from "./module/clock.js";
 
-let clock_edit = document.getElementById("clock_edit");
 const play_start = document.getElementById("play_start");
 const play_btn = document.getElementById("play");
+const clock_select = document.getElementById("clock_select");
+const clock_select_btn = document.getElementsByClassName("clock_select_btn");
+const clock_select_btn_is_current = document.getElementsByClassName("clock_select_btn is_current")[0];
+
+let start;
+let interval_id;
+let clock_edit;
+let now_time;
+let time;
+
+console.log(clock_select_btn);
+console.log(clock_select_btn_is_current);
+
+clock_edit = document.getElementById("clock_edit");
+// console.log(clock_edit);
+
+now_time = clock_edit.innerHTML;
+time = parseInt(now_time) * 60 - 1;
+// console.log(time);
 
 toggle_select();
 select_time();
 
-let start;
-let interval_id;
+clock_edit = document.getElementById("clock_edit");
+// console.log(clock_edit);
+now_time = clock_edit.innerHTML;
+time = parseInt(now_time) * 60 - 1;
+// console.log(time);
+
+// console.log(time);
 
 play_start.addEventListener("click", () => {
   if (play_btn.classList[2] === "fa-play-circle") {
-    // start = new Date();
-    let time = parseInt(clock_edit.innerHTML) * 60 - 1;
-    console.log(time);
-
-    // interval_id = setInterval(goClock, 1000);
-
-    interval_id = setInterval(() => {
-      let seconds = Math.floor(time % 60);
-      let minutes = Math.floor(time / 60);
-
-      seconds = addZero(seconds);
-      minutes = addZero(minutes);
-
-      console.log(time);
-      console.log(seconds);
-      console.log(minutes);
-
-      clock_edit.innerHTML = minutes + ":" + seconds;
-      time--;
-   }, 1000);
+    start = new Date();
+    // console.log(clock_select_btn[0]);
+    // clock_edit = document.getElementById("clock_edit");
+    // time = parseInt(clock_edit.innerHTML);
+    // console.log(clock_edit);
+    // console.log(now_time);
 
     // pauseボタンに切り替え
     play_btn.classList.remove("fa-play-circle");
     play_btn.classList.add("fa-pause-circle");
+
+    // interval_id = setInterval(goClock, 1000);
+    interval_id = setInterval(clockTime, 1000);
 
   } else {
     clearInterval(interval_id);
@@ -51,6 +63,27 @@ const addZero = (value) => {
     value = "0" + value;
   }
   return value;
+}
+
+const clockTime = () => {
+  let minutes = Math.floor(time / 60);
+  let seconds = Math.floor(time % 60);
+
+  seconds = addZero(seconds);
+  minutes = addZero(minutes);
+
+  clock_edit.innerHTML = minutes + ":" + seconds;
+  // clock_edit = document.getElementById("clock_edit");
+  // now_time = clock_edit.innerHTML;
+  // console.log(clock_edit);
+  // console.log(now_time);
+
+  if (parseInt(clock_edit.innerHTML) >= 0) {
+    time--;
+  } else {
+    clearInterval(interval_id);
+    time;
+  }
 }
 
 // const goClock = () => {
--- module/clock.js
+++ module/clock.js
@@ -1,6 +1,12 @@
-const clock_edit = document.getElementById("clock_edit");
 const clock_select = document.getElementById("clock_select");
 const clock_select_btn = document.getElementsByClassName("clock_select_btn");
+const clock_select_btn_is_current = document.getElementsByClassName("clock_select_btn is_current");
+
+let clock_edit;
+let now_time;
+let time;
+
+clock_edit = document.getElementById("clock_edit");
 
 const toggle_is_open = () => {
   clock_select.classList.toggle("clock_select");
@@ -19,71 +25,101 @@ const select_time = () => {
   clock_select_btn[0].addEventListener("click", () => {
     let select_time = clock_select_btn[0].innerHTML;
     clock_edit.innerHTML = select_time;
+    clock_select_btn_is_current[0].classList.remove("is_current");
+    clock_select_btn[0].classList.add("is_current");
+    // clock_edit = document.getElementById("clock_edit");
+    // now_time = clock_edit.innerHTML;
+    // time = parseInt(now_time) * 60 - 1;;
+    // console.log(time);
     toggle_is_open();
   });
   
   clock_select_btn[1].addEventListener("click", () => {
     let select_time = clock_select_btn[1].innerHTML;
     clock_edit.innerHTML = select_time;
+    clock_select_btn_is_current[0].classList.remove("is_current");
+    clock_select_btn[1].classList.add("is_current");
     toggle_is_open();
   });
   
   clock_select_btn[2].addEventListener("click", () => {
     let select_time = clock_select_btn[2].innerHTML;
     clock_edit.innerHTML = select_time;
+    clock_select_btn_is_current[0].classList.remove("is_current");
+    clock_select_btn[2].classList.add("is_current");
     toggle_is_open();
   });
   
   clock_select_btn[3].addEventListener("click", () => {
     let select_time = clock_select_btn[3].innerHTML;
     clock_edit.innerHTML = select_time;
+    clock_select_btn_is_current[0].classList.remove("is_current");
+    clock_select_btn[3].classList.add("is_current");
     toggle_is_open();
   });
   
   clock_select_btn[4].addEventListener("click", () => {
     let select_time = clock_select_btn[4].innerHTML;
     clock_edit.innerHTML = select_time;
+    clock_select_btn_is_current[0].classList.remove("is_current");
+    clock_select_btn[4].classList.add("is_current");
     toggle_is_open();
   });
   
   clock_select_btn[5].addEventListener("click", () => {
     let select_time = clock_select_btn[5].innerHTML;
     clock_edit.innerHTML = select_time;
+    clock_select_btn_is_current[0].classList.remove("is_current");
+    clock_select_btn[5].classList.add("is_current");
     toggle_is_open();
   });
   
   clock_select_btn[6].addEventListener("click", () => {
     let select_time = clock_select_btn[6].innerHTML;
     clock_edit.innerHTML = select_time;
+    clock_select_btn_is_current[0].classList.remove("is_current");
+    clock_select_btn[6].classList.add("is_current");
     toggle_is_open();
   });
   
   clock_select_btn[7].addEventListener("click", () => {
     let select_time = clock_select_btn[7].innerHTML;
     clock_edit.innerHTML = select_time;
+    clock_select_btn_is_current[0].classList.remove("is_current");
+    clock_select_btn[7].classList.add("is_current");
     toggle_is_open();
   });
   
   clock_select_btn[8].addEventListener("click", () => {
     let select_time = clock_select_btn[8].innerHTML;
     clock_edit.innerHTML = select_time;
+    clock_select_btn_is_current[0].classList.remove("is_current");
+    clock_select_btn[8].classList.add("is_current");
     toggle_is_open();
   });
   
   clock_select_btn[9].addEventListener("click", () => {
     let select_time = clock_select_btn[9].innerHTML;
     clock_edit.innerHTML = select_time;
+    clock_select_btn_is_current[0].classList.remove("is_current");
+    clock_select_btn[9].classList.add("is_current");
     toggle_is_open();
   });
   
   clock_select_btn[10].addEventListener("click", () => {
     let select_time = clock_select_btn[10].innerHTML;
     clock_edit.innerHTML = select_time;
+    clock_select_btn_is_current[0].classList.remove("is_current");
+    clock_select_btn[10].classList.add("is_current");
     toggle_is_open();
   });
+  // clock_edit = document.getElementById("clock_edit");
+  // now_time = clock_edit.innerHTML;
+  // time = parseInt(now_time) * 60 - 1;;
+  // console.log(time);
 };
 
-export { toggle_select, select_time };
+export { toggle_is_open, toggle_select, select_time };
 
 
 
