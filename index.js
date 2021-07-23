78f36ddfe5efdea7b063d804d9f757d840d70051
yait-ucess <bandai28@ezweb.ne.jp>
2021-07-23 12:14:31 +0900

    ⏹  ボタンをクリックするとタイマーが止まって、トップページに遷移する機能を実装。

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import { toggle_select, select_time } from "./module/clock.js";
 
 const play_start = document.getElementById("play_start");
 const play_btn = document.getElementById("play");
+const play_stop = document.getElementById("play_stop");
+const stop_btn = document.getElementById("stop");
 
 let interval_id;
 let clock_edit;
@@ -53,11 +55,8 @@ const clockTime = () => {
   clock_edit.innerHTML = minutes + ":" + seconds;
 
   if (clock_edit.innerHTML !== "00:00") {
-    console.log("経過");
-    console.log(clock_edit.innerHTML);
     time--;
   } else {
-    console.log("ストップ");
     time;
     clearInterval(interval_id);
 
@@ -65,3 +64,15 @@ const clockTime = () => {
     play_btn.classList.add("fa-play-circle");
   }
 }
+
+play_stop.addEventListener("click", () => {
+  clearInterval(interval_id);
+
+  if (play_btn.classList[2] === "fa-pause-circle") {
+    play_btn.classList.remove("fa-pause-circle");
+    play_btn.classList.add("fa-play-circle");
+  }
+
+  clock_is_current = document.getElementsByClassName("clock_select_btn is_current")[0];
+  clock_edit.innerHTML = clock_is_current.innerHTML;
+});
