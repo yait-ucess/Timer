640daeaed371437242e624e74bbafdc2174e9023
yait-ucess <bandai28@ezweb.ne.jp>
2021-07-27 16:46:04 +0900

    skip時に、画面全体の色が変わるようにする（未完了）

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 import { toggle_select, select_time } from "./module/clock.js";
 
+const three_dot = document.getElementsByClassName("three_dot_focus");
+const clock = document.getElementById("clock");
+const clock_text = document.getElementById("clock_text");
 const play_start = document.getElementById("play_start");
 const play_btn = document.getElementById("play");
 const play_stop = document.getElementById("play_stop");
 const stop_btn = document.getElementById("stop");
+const play_skip = document.getElementById("play_skip");
+const skip_btn = document.getElementById("skip");
 
 let interval_id;
 let clock_edit;
@@ -80,7 +85,6 @@ const clockTime = () => {
 
 play_stop.addEventListener("click", () => {
   clearInterval(interval_id);
-  console.log(play_btn.classList.contains("fa-pause-circle"));
 
   if (play_btn.classList.contains("fa-pause-circle")) {
     play_btn.classList.remove("fa-pause-circle");
@@ -89,4 +93,46 @@ play_stop.addEventListener("click", () => {
 
   clock_is_current = document.getElementsByClassName("clock_select_btn is_current")[0];
   clock_edit.innerHTML = clock_is_current.innerHTML;
+});
+
+play_skip.addEventListener("click", () => {
+  if (play_skip.classList.contains("focus")) {
+    play_skip.classList.remove("focus");
+    play_skip.classList.add("rest");
+  }
+  else if (play_skip.classList.contains("rest")) {
+    play_skip.classList.remove("rest");
+    play_skip.classList.add("focus");
+  }
+
+  if (play_skip.classList.contains("rest")) {
+    document.body.style.background = "#707070";
+    clock_edit.style.color = "#F7C100";
+
+    if (clock.classList.contains("clock_focus")) {
+      clock.classList.remove("clock_focus");
+      clock.classList.add("clock_rest");
+    }
+    else if (clock.classList.contains("clock_rest")) {
+      clock.classList.remove("clock_rest");
+      clock.classList.add("clock_focus");
+    }
+  }
+
+  clock_text.style.color = "#F7C100";
+
+  skip_btn.classList.remove("fas_skip_focus");
+  skip_btn.classList.add("fas_skip_rest");
+  
+  stop_btn.classList.remove("fas_stop_focus");
+  stop_btn.classList.add("fas_stop_rest");
+  
+  play_btn.classList.remove("fas_play_focus");
+  play_btn.classList.add("fas_play_rest");
+
+  console.log(three_dot.length);
+  for (let i = 0; three_dot.length; i++) {
+    three_dot[i].classList.remove("three_dot_focus");
+    three_dot[i].classList.add("three_dot_rest");
+  }
 });
